da9fb53e7c803b1a8d8517a8a43c6bd95c8d94c7
bartoszl <barteklewandowski94@gmail.com>
2016-10-14 22:55:17 +0100

    server entry point

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');  
+var methodOverride = require('method-override');
+
+var app = express();
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var api = require('./server/routes/api');
+app.use('/api', api);
+
+app.use('/', function(req, res, next){
+  res.sendFile('./public/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Application listening on port 3000");
+});
