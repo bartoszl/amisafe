9607fef0ec6a6bb63df4ac757e83de6db5633c01
Roman O'Dowd <roman.odowd@hotmail.de>
2016-10-15 03:17:51 +0100

    added lookup crimes by zone method

--- controllers/chicagoFlowController.js
+++ controllers/chicagoFlowController.js
@@ -11,8 +11,9 @@ chicagoFlowController.prototype.lookUpData = function(type){
           params: {
             "$where": "date between '2016-01-01T00:00:00' and '2016-06-30T23:59:59'",
             "domestic": false,
+            "$limit" : 100
             // "arrest": true,
-            "primary_type": type
+            // "primary_type": type
           }
     });
 
@@ -25,6 +26,17 @@ chicagoFlowController.prototype.lookUpZone = function(type, lat, long, rad){
             "domestic": false,
             // "arrest": true,
             "primary_type": type
+          }
+    });
+};
+
+chicagoFlowController.prototype.lookUpCrimeInZone = function(lat, long, rad){
+  return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
+          params: {
+            "$where": "date between '2016-01-01T00:00:00' and '2016-01-30T23:59:59' and within_circle(location, " + lat + ", " + long + ", " + rad + ")",
+            "domestic": false,
+            // "arrest": true,
+
           }
     });
 };
@@ -46,7 +58,19 @@ chicagoFlowController.prototype.getInfo = function(type, description, arrested, 
 chicagoFlowController.prototype.getCrimeCount = function(type, lat, long, rad){
   var dataPromise = this.lookUpZone(type, lat, long, rad);
   dataPromise.then(function(result){
-      console.log("Crime count: " + result.data.length);
+      console.log(type + " count: " + result.data.length);
+  }).catch(function (error) {
+    console.log(error);
+  });
+};
+
+chicagoFlowController.prototype.getCrimeByLocation = function(lat, long, rad){
+  var dataPromise = this.lookUpCrimeInZone(lat, long, rad);
+  dataPromise.then(function(result){
+    console.log("Crime Variety Count: " + result.data.length);
+    result.data.map(function(entry){
+      console.log("Crime Variety: " + entry.description);
+    });
   }).catch(function (error) {
     console.log(error);
   });
--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,9 @@
 var helper = require('./chicagoFlowController.js');
 
+
+
 var dataHelper = new chicagoFlowController();
 // dataHelper.getInfo("NARCOTICS", "description");
-dataHelper.getInfo("HOMICIDE", "description", "arrest", "location");
+// dataHelper.getInfo("HOMICIDE", "description", "arrest", "location");
 dataHelper.getCrimeCount("THEFT", 41.86, -87.66, 250);
+dataHelper.getCrimeByLocation(41.86, -87.66, 250);
