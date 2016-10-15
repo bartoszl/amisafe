c223e3fa76d499c5767d0237a94da998fa6706cb
Roman O'Dowd <roman.odowd@hotmail.de>
2016-10-15 01:38:07 +0100

    added method to check a number of a given crime inside a given radius

--- controllers/chicagoFlowController.js
+++ controllers/chicagoFlowController.js
@@ -18,16 +18,36 @@ chicagoFlowController.prototype.lookUpData = function(type){
 
 };
 
+chicagoFlowController.prototype.lookUpZone = function(type, lat, long, rad){
+  return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
+          params: {
+            "$where": "date between '2016-01-01T00:00:00' and '2016-01-30T23:59:59' and within_circle(location, " + lat + ", " + long + ", " + rad + ")",
+            "domestic": false,
+            // "arrest": true,
+            "primary_type": type
+          }
+    });
+};
 
-chicagoFlowController.prototype.getInfo = function(type, printType1, printType2){
+
+chicagoFlowController.prototype.getInfo = function(type, description, arrested, location){
 
     var dataPromise = this.lookUpData(type);
     dataPromise.then(function(result){
       result.data.map(function(entry){
-        console.log("Description: " + entry[printType1], "Arrested: " + entry[printType2]);
+        console.log("Description: " + entry[description], "Arrested: " + entry[arrested], "Location: " + entry[location]);
       });
     }).catch(function (error) {
       console.log(error);
     });
 
+};
+
+chicagoFlowController.prototype.getCrimeCount = function(type, lat, long, rad){
+  var dataPromise = this.lookUpZone(type, lat, long, rad);
+  dataPromise.then(function(result){
+      console.log("Crime count: " + result.data.length);
+  }).catch(function (error) {
+    console.log(error);
+  });
 };
--- controllers/test.js
+++ controllers/test.js
@@ -2,4 +2,5 @@ var helper = require('./chicagoFlowController.js');
 
 var dataHelper = new chicagoFlowController();
 // dataHelper.getInfo("NARCOTICS", "description");
-dataHelper.getInfo("HOMICIDE", "description", "arrest");
+dataHelper.getInfo("HOMICIDE", "description", "arrest", "location");
+dataHelper.getCrimeCount("THEFT", 41.86, -87.66, 250);
