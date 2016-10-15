bfb64a0a9801e85a084790fb269524aeb9632689
Roman O'Dowd <roman.odowd@hotmail.de>
2016-10-15 04:01:40 +0100

    Added more varied functionality: specific types info and general-all types info for different situations

--- controllers/chicagoFlowController.js
+++ controllers/chicagoFlowController.js
@@ -1,23 +1,46 @@
 var axios = require('axios');
 
 chicagoFlowController = function(){
-  // this.getDescription("NARCOTICS");
-  // this.getDistrict("NARCOTICS");
+
 };
 
+/*
+  look up all crimes over a specific timespan
+*/
 
-chicagoFlowController.prototype.lookUpData = function(type){
+chicagoFlowController.prototype.lookUpAllOverPeriod = function(startDate, stopDate, limit){
   return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
           params: {
-            "$where": "date between '2016-01-01T00:00:00' and '2016-06-30T23:59:59'",
+            "$where": "date between \'" +startDate+ "\' and \'"+stopDate+"\'",
             "domestic": false,
-            "$limit" : 100
+            "$limit" : limit
             // "arrest": true,
-            // "primary_type": type
+
           }
     });
 
 };
+
+/*
+  look up a specific crimes over a specific timespan
+*/
+
+chicagoFlowController.prototype.lookUpCrimeOverPeriod = function(type, startDate, stopDate, limit){
+  return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
+          params: {
+            "$where": "date between \'" +startDate+ "\' and \'"+stopDate+"\'",
+            "domestic": false,
+            "$limit" : limit
+            // "arrest": true,
+            "primary_type": type
+          }
+    });
+
+};
+
+/*
+  look up crime type by location (useful for getting the number of crimes)
+*/
 
 chicagoFlowController.prototype.lookUpZone = function(type, lat, long, rad){
   return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
@@ -30,6 +53,10 @@ chicagoFlowController.prototype.lookUpZone = function(type, lat, long, rad){
     });
 };
 
+/*
+  look up all crimes in a location
+*/
+
 chicagoFlowController.prototype.lookUpCrimeInZone = function(lat, long, rad){
   return axios.get("https://data.cityofchicago.org/resource/6zsd-86xi.json", {
           params: {
@@ -41,19 +68,46 @@ chicagoFlowController.prototype.lookUpCrimeInZone = function(lat, long, rad){
     });
 };
 
+/*
+  displays crimes with attributes of our chosing over a timespan
+  crime types are defined in a measurementTypes array
+*/
 
-chicagoFlowController.prototype.getInfo = function(type, description, arrested, location){
+chicagoFlowController.prototype.getInfo = function(startDate, stopDate, measurementTypes, limit){
 
-    var dataPromise = this.lookUpData(type);
+    var dataPromise = this.lookUpAllOverPeriod(startDate, stopDate, limit);
     dataPromise.then(function(result){
       result.data.map(function(entry){
-        console.log("Description: " + entry[description], "Arrested: " + entry[arrested], "Location: " + entry[location]);
+
+        console.log("\nDescription: " + entry[measurementTypes[0]], "\nArrested: " + entry[measurementTypes[1]], "\nLocation: " + entry[measurementTypes[2]]);
       });
     }).catch(function (error) {
       console.log(error);
     });
 
 };
+
+/*
+  displays all crimes of a specific type over a timespan
+*/
+
+chicagoFlowController.prototype.getSpecificInfo = function(type, startDate, stopDate, limit){
+
+    var dataPromise = this.lookUpAllOverPeriod(type, startDate, stopDate, limit);
+    dataPromise.then(function(result){
+      result.data.map(function(entry){
+
+        console.log("\nDescription: " + entry[type]);
+      });
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+};
+
+/*
+  displays the amount of a specific crime type in a given location and radius
+*/
 
 chicagoFlowController.prototype.getCrimeCount = function(type, lat, long, rad){
   var dataPromise = this.lookUpZone(type, lat, long, rad);
@@ -63,6 +117,10 @@ chicagoFlowController.prototype.getCrimeCount = function(type, lat, long, rad){
     console.log(error);
   });
 };
+
+/*
+  displays all crimes in a given location and radius
+*/
 
 chicagoFlowController.prototype.getCrimeByLocation = function(lat, long, rad){
   var dataPromise = this.lookUpCrimeInZone(lat, long, rad);
--- controllers/test.js
+++ controllers/test.js
@@ -3,7 +3,15 @@ var helper = require('./chicagoFlowController.js');
 
 
 var dataHelper = new chicagoFlowController();
+var startDate = '2016-01-01T00:00:00';
+var stopDate = '2016-06-30T23:59:59';
+var limit = 50;
 // dataHelper.getInfo("NARCOTICS", "description");
-// dataHelper.getInfo("HOMICIDE", "description", "arrest", "location");
+
 dataHelper.getCrimeCount("THEFT", 41.86, -87.66, 250);
 dataHelper.getCrimeByLocation(41.86, -87.66, 250);
+
+var measurementTypes = ["description", "arrest", "location"];
+console.log("/****************Crimes Committed in Chicago from " + startDate + " till " + stopDate + "****************/");
+dataHelper.getInfo(startDate, stopDate, measurementTypes, limit);
+dataHelper.getSpecificInfo(type, startDate, stopDate, measurementTypes, limit);
